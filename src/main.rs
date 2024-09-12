93f20a915a7de06537679bded6432ba20970f7b8
Petri Cavalry <maximizemanhood@outlook.com>
2024-09-12 21:29:10 +0800

    fix: read file just written after download wordlist

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,42 @@ use directories::ProjectDirs;
 use rand::{seq::SliceRandom, Rng};
 use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
-use std::io::{self, BufRead};
 use std::path::Path;
 
 mod cli;
+
+const URL: &str = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt";
+
+async fn download_wordlist<T, P>(url: T, path: P) -> Result<String, reqwest::Error>
+where
+    T: reqwest::IntoUrl,
+    P: AsRef<Path>,
+{
+    let content = reqwest::get(url).await?.text().await?;
+    let mut file = File::create(&path).expect("failed to create directory");
+    file.write_all(content.as_bytes())
+        .expect("failed to write content");
+    Ok(content)
+}
 
 async fn get_wordlist<T, P>(url: T, path: P) -> Result<Vec<String>, io::Error>
 where
     T: reqwest::IntoUrl,
     P: AsRef<Path>,
 {
-    if !path.as_ref().exists() {
-        let resp = reqwest::get(url)
-            .await
-            .expect("fail to download wordlist")
-            .text()
-            .await
-            .expect("fail to get response");
-        let mut file = File::create(&path).expect("fail to create wordlist file");
-        file.write_all(resp.as_bytes()).expect("fail to write file");
-    }
-
-    let file = std::fs::File::open(path)?;
-    let lines = io::BufReader::new(file).lines();
+    let content = if path.as_ref().exists() {
+        fs::read_to_string(path)?
+    } else {
+        download_wordlist(url, path).await.unwrap()
+    };
     let mut wordlist = Vec::new();
-    for line in lines {
+    for line in content.lines() {
         wordlist.push(
-            line?
-                .split_whitespace()
+            line.split_whitespace()
                 .last()
-                .expect("cannot read line")
+                .expect("failed to get last word from line")
                 .to_string(),
         );
     }
@@ -43,13 +48,15 @@ where
 #[tokio::main]
 async fn main() {
     let proj_dir = ProjectDirs::from("com.github", "petricavalry", "mowereligible")
-        .expect("fail to get configuration path");
+        .expect("failed to get configuration path");
     let config_dir = proj_dir.config_dir();
-    fs::create_dir_all(config_dir).expect("fail to create configuration path");
+    fs::create_dir_all(config_dir).expect("failed to create configuration path");
 
     let path = config_dir.join("eff_large_wordlist.txt");
-    let url = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt";
-    let wordlist = get_wordlist(url, path).await.expect("unknown");
+
+    let wordlist = get_wordlist(URL, path)
+        .await
+        .expect("failed to download EFF Large Wordlist");
 
     let args = cli::Cli::parse();
 
