1ae3838d51423f1361564e4fd3f5c60c0e76e6d1
Petri Cavalry <maximizemanhood@outlook.com>
2024-09-12 19:06:24 +0800

    feat: download EFF Large Wordlist if not exists

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 
 A passphrase generator write in Rust.
 
-## Prerequires
+It will download
+[EFF Large Wordlist](https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt)
+if not exists.
 
-Download EFF large worldlist first.
-
-```bash
-curl https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt -o eff_large_wordlist.txt
-```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use clap::Parser;
-use mowereligibl::cli::{Cli, Commands};
-use rand::{Rng, seq::SliceRandom};
+use directories::ProjectDirs;
+use rand::{seq::SliceRandom, Rng};
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn wordlist<T>(t: T) -> io::Result<Vec<String>>
+mod cli;
+
+async fn get_wordlist<T, P>(url: T, path: P) -> Result<Vec<String>, io::Error>
 where
-    T: AsRef<Path>,
+    T: reqwest::IntoUrl,
+    P: AsRef<Path>,
 {
-    let file = std::fs::File::open(t)?;
+    if !path.as_ref().exists() {
+        let resp = reqwest::get(url)
+            .await
+            .expect("fail to download wordlist")
+            .text()
+            .await
+            .expect("fail to get response");
+        let mut file = File::create(&path).expect("fail to create wordlist file");
+        file.write_all(resp.as_bytes()).expect("fail to write file");
+    }
+
+    let file = std::fs::File::open(path)?;
     let lines = io::BufReader::new(file).lines();
     let mut wordlist = Vec::new();
     for line in lines {
@@ -23,16 +40,25 @@ where
     Ok(wordlist)
 }
 
-fn main() {
-    let args = Cli::parse();
-    let wordlist = wordlist("eff_large_wordlist.txt").expect("");
+#[tokio::main]
+async fn main() {
+    let proj_dir = ProjectDirs::from("com.github", "petricavalry", "mowereligible")
+        .expect("fail to get configuration path");
+    let config_dir = proj_dir.config_dir();
+    fs::create_dir_all(config_dir).expect("fail to create configuration path");
+
+    let path = config_dir.join("eff_large_wordlist.txt");
+    let url = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt";
+    let wordlist = get_wordlist(url, path).await.expect("unknown");
+
+    let args = cli::Cli::parse();
 
     match args.command {
-        Commands::Number(opts) => {
+        cli::Commands::Number(opts) => {
             let number = rand::thread_rng().gen_range(opts.start..opts.end);
             println!("{}", number);
         }
-        Commands::String(opts) => {
+        cli::Commands::String(opts) => {
             let mut words: Vec<String> = vec![];
             for _ in 0..opts.length {
                 words.push(
@@ -44,5 +70,5 @@ fn main() {
             }
             println!("{}", words.join(&opts.separator));
         }
-    }
+    };
 }
